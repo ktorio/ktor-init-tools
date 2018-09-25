c51ac40242c500e52348283b652f5d416f1d4591
soywiz <soywiz@gmail.com>
2018-09-25 15:42:20 +0200

    Common tests should also work

---
+++ ktor-generator/src/commonTest/kotlin/io/ktor/start/util/MyCommonTest.kt
@@ -0,0 +1,11 @@
+package io.ktor.start.util
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class MyCommonTest {
+    @Test
+    fun commonTest() {
+        assertEquals(1, 1)
+    }
+}
