3246f8119a43cddcbf46d19cfbe617064cec3712
Leonid Stashevsky <leonid.stashevsky@gmail.com>
2021-07-01 12:44:00 +0300

    Prepare new generator deployment

--- ktor-intellij-plugin/src/io/ktor/start/intellij/locations/PatternFileTypeFactory.kt
+++
@@ -1,9 +0,0 @@
-package io.ktor.start.intellij.locations
-
-import com.intellij.openapi.fileTypes.*
-
-class PatternFileTypeFactory : FileTypeFactory() {
-    override fun createFileTypes(consumer: FileTypeConsumer) {
-        consumer.consume(PatternFileType)
-    }
-}
